5a03789f9b4ce66a034de5d9f57706e0d8517ffd
Martin Albert <martin.albert.187@gmail.com>
2020-06-14 15:23:17 +0200

    created two subclasses for BaseController to handle GET & POST to /image

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,61 @@
+import express, { Request, Response } from 'express'
+import { BaseController } from './BaseController'
+import Parser from '../../../coords-parse/dist'
+
+interface IRecordService {
+    id: number,
+    imageName: string,
+    lat: number,
+    long: number,
+    alt: number
+}
+
+export class CreateRecordController extends BaseController {
+
+    private recordService: IRecordService
+
+    constructor (recordService: IRecordService) {
+        super()
+        this.recordService = recordService
+    }
+
+    protected async executeImpl (req: Request, res: Response): Promise<void | any> {
+        try {
+            // handle request
+            // console.log('req.file:', req.file)
+            const imageBuffer: Buffer = req.file.buffer
+            const coordsParser = new Parser(imageBuffer)
+
+            console.log('after parse', coordsParser.coordinates)
+            res.status(200).send('ok')
+
+            // save record
+
+        } catch (err) {
+            return this.fail(res, err.toString())
+        }
+    }
+}
+
+export class GetRecordController extends BaseController {
+    
+    private recordService: IRecordService
+
+    constructor (recordService: IRecordService) {
+        super()
+        this.recordService = recordService
+    }
+
+    protected async executeImpl (req: Request, res: Response): Promise<void | any> {
+        try {
+            // handle request
+            const image = req.body
+            res.status(200).send(image)
+
+            // save record
+
+        } catch (err) {
+            return this.fail(res, err.toString())
+        }
+    }
+}
