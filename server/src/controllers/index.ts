5b4be4cd199c5cd9f6e8fc829c64a493205c18b5
Martin Albert <martin.albert.187@gmail.com>
2020-07-08 11:40:06 +0200

    updated semantics and routes

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,58 +1,54 @@
 import express, { Request, Response } from 'express'
 import { BaseController } from './BaseController'
-import Parser from '../../../coords-parse/dist'
+import Parser from '../util/coords-parse'
 
 interface IRecordService {
-    id: number,
-    imageName: string,
-    lat: number,
-    long: number,
+    id: number
+    imageName: string
+    lat: number
+    long: number
     alt: number
 }
 
-export class CreateRecordController extends BaseController {
-
+export class CreateImageController extends BaseController {
     private recordService: IRecordService
 
-    constructor (recordService: IRecordService) {
+    constructor(recordService: IRecordService) {
         super()
         this.recordService = recordService
     }
 
-    protected async executeImpl (req: Request, res: Response): Promise<void | any> {
+    protected async executeImpl(req: Request, res: Response): Promise<void | any> {
         try {
             // handle request
             const imageBuffer: Buffer = req.file.buffer
-            const coordsParser = new Parser(imageBuffer)
+            const coordsParser: Parser = new Parser(imageBuffer)
 
             console.log('after parse', coordsParser.coordinates)
             res.status(200).send(coordsParser.coordinates)
 
             // save record
-
         } catch (err) {
             return this.fail(res, err.toString())
         }
     }
 }
 
-export class GetRecordController extends BaseController {
-    
+export class GetImageController extends BaseController {
     private recordService: IRecordService
 
-    constructor (recordService: IRecordService) {
+    constructor(recordService: IRecordService) {
         super()
         this.recordService = recordService
     }
 
-    protected async executeImpl (req: Request, res: Response): Promise<void | any> {
+    protected async executeImpl(req: Request, res: Response): Promise<void | any> {
         try {
             // handle request
             const image = req.body
             res.status(200).send(image)
 
             // save record
-
         } catch (err) {
             return this.fail(res, err.toString())
         }
