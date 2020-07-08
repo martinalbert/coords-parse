8d4314555a73372ed372ac21fdd608be4942d3fa
Martin Albert <martin.albert.187@gmail.com>
2020-07-08 15:05:15 +0200

    updated server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,24 @@
-// require dotenv to access enviroment ( process.env ) variables
-import 'dotenv/config'
-
 import http from 'http'
+import config from './config'
 import app from './app'
+import { connect } from './db'
+import seed from './db/repos/postgres/seed'
 
-const PORT = process.env.PORT || 3000
+const PORT = config.PORT || 3000
 const server = http.createServer(app)
 
 server.listen(PORT, () => {
     console.log('server started on port: ' + PORT)
+
+    // connect to DB (postgres)
+    connect()
+
+    // seed sample data
+    seed()
+        .then(() => console.log('data have been seeded'))
+        .catch(err => {
+            throw Error(err)
+        })
 })
 
 server.on('error', (err: Error) => {
