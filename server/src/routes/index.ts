39cdfbcd78b387af0ad92c0ef1df64a1f61d7237
Martin Albert <martin.albert.187@gmail.com>
2020-07-09 11:17:43 +0200

    added /images endpoint rouote and updated semantics

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,34 +1,69 @@
-import express, { Router, Request, Response } from 'express'
+import express from 'express'
 import multer from 'multer'
 import checkAuth from '../util/checkAuth'
 import { register, login, getAllUsers } from '../useCases/UserUseCases'
+import {
+    getImageByID,
+    getAllImages,
+    createImage,
+    deleteImage,
+} from '../useCases/ImageUseCases'
 
+/**
+ * Multer implementation of storage to store files in memoory
+ * @constant storage
+ */
 const storage = multer.memoryStorage()
+
+/**
+ * Multer instance for processing files
+ * @constant upload
+ * @instance
+ */
 const upload = multer({
     storage: storage,
 })
 
-const router: Router = express.Router()
+/**
+ * Express router that creates mountable route handlers
+ * @constant router
+ */
+const router = express.Router()
 
-// router.get('/image', (req: Request, res: Response) => getImageController.exec(req, res))
-// router.post('/image', upload.single('image'), (req: Request, res: Response) =>
-//     createImageController.exec(req, res)
-// )
+/**
+ * endpoint /users
+ */
 router.get(
     '/users',
-    (req, res, next) => checkAuth,
-    async (req, res, next) => await getAllUsers.exec
+    checkAuth,
+    async (req, res, next) => await getAllUsers.exec(req, res, next)
 )
-router.post('/user/register', async (req, res, next) => await register.exec)
-router.post('/user/login', async (req, res, next) => await login.exec)
+router.post('/user/register', async (req, res, next) => await register.exec(req, res, next))
+router.post('/user/login', async (req, res, next) => await login.exec(req, res, next))
+
+/**
+ * endpoint /images
+ */
+router.get(
+    '/images',
+    checkAuth,
+    async (req, res, next) => await getAllImages.exec(req, res, next)
+)
+router.get(
+    '/images/:id',
+    checkAuth,
+    async (req, res, next) => await getImageByID.exec(req, res, next)
+)
+router.post(
+    '/images',
+    checkAuth,
+    upload.single('image'),
+    async (req, res, next) => await createImage.exec(req, res, next)
+)
+router.delete(
+    '/images/:id',
+    checkAuth,
+    async (req, res, next) => await deleteImage.exec(req, res, next)
+)
 
 export default router
-
-// need to implement fileFilter
-// check file extension for compatibility with ExifReader
-// File type	Exif	IPTC	XMP	    ICC	    Thumbnail
-// JPEG	        yes	    yes	    yes	    yes	    yes
-// TIFF	        yes	    yes	    yes	    yes	    no
-// PNG	        no	    no	    yes	    no	    no
-// HEIC/HEIF	yes	    no	    no	    yes	    no
-// WebP	        yes	    no	    yes	    yes	    yes
