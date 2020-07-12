fd76292b5b0b7a9ed5b7bc6be19a6dd368a9fa2d
Martin Albert <martin.albert.187@gmail.com>
2020-07-12 10:33:42 +0200

    split the seed functions into two async functions

--- server/src/db/repos/postgres/seed.ts
+++ server/src/db/repos/postgres/seed.ts
@@ -10,6 +10,28 @@ const imageRepo = new ImageRepo()
 const users = data.users
 const images = data.images
 
+const seedUsers = async (): Promise<void> => {
+    for (const user of users) {
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw new Error(err.message)
+
+            bcrypt.hash(user.password, salt, async (err, hash) => {
+                if (err) throw new Error(err.message)
+
+                // replace password with hash
+                user.password = hash
+
+                // save record
+                await userRepo.register(user)
+            })
+        })
+    }
+}
+
+const seedImages = async (): Promise<void> => {
+    for (const image of images) await imageRepo.create(image)
+}
+
 /**
  * Function that seed base two Users Applifting and Batman\
  * and base few images that can be monitored.
@@ -21,26 +43,10 @@ const seed = async (): Promise<void> => {
         const { imageCount, userCount } = await getLastID()
 
         // seed users
-        if (userCount < users.length)
-            for (const user of users) {
-                bcrypt.genSalt(10, (err, salt) => {
-                    if (err) throw new Error(err.message)
-
-                    bcrypt.hash(user.password, salt, async (err, hash) => {
-                        if (err) throw new Error(err.message)
-
-                        // replace password with hash
-                        user.password = hash
-
-                        // save record
-                        await userRepo.register(user)
-                    })
-                })
-            }
+        if (userCount < users.length) await seedUsers()
 
         // seed images to monitor
-        if (imageCount < images.length)
-            for (const image of images) await imageRepo.create(image)
+        if (imageCount < images.length) await seedImages()
     } catch (error) {
         throw Error(error)
     }
