fdeb46c398f73678696a2a2f83ba5aaaf2321cf1
Martin Albert <martin.albert.187@gmail.com>
2020-07-15 10:56:27 +0200

    split and fixed functionality of postgres sequences

--- server/src/db/repos/postgres/actions/index.ts
+++ server/src/db/repos/postgres/actions/index.ts
@@ -28,22 +28,44 @@ export const dropAllTables = () => {
 
 /**
  * Helper Function\
- * Function that gets the last ID of users and images.
+ * Function that sets the postgres sequence ID to ID of last record.
  *
- * @async @function getLastID
- * @return {Object} object with user last ID and endpoint last ID
+ * @async @function resetSequence
+ * @param  {string} tablename - name of table
  */
-export const getLastID = async () => {
-    const imageCount = await sequelize.query(
-        'SELECT setval(\'images_id_seq\', (SELECT MAX(id) FROM "images"));',
+export const resetSequence = async (tablename: string) => {
+    await sequelize.query(
+        `SELECT setval(pg_get_serial_sequence('${tablename}', 'id'), coalesce(max(id), 1), true) FROM \"${tablename}\";`,
         { type: QueryTypes.SELECT }
     )
+}
+
+/**
+ * Helper Function\
+ * Function that gets the largest ID of Users in database.
+ *
+ * @async @function getLastUserID
+ * @return {number} number of user last ID
+ */
+export const getLastUserID = async () => {
     const userCount = await sequelize.query(
-        'SELECT setval(\'users_id_seq\', (SELECT MAX(id) FROM "users"));',
+        'SELECT coalesce(max(id), 0) AS count FROM "users";',
         { type: QueryTypes.SELECT }
     )
-    return {
-        imageCount: Number(imageCount[0].setval),
-        userCount: Number(userCount[0].setval),
-    }
+    return userCount[0].count
+}
+
+/**
+ * Helper Function\
+ * Function that gets the largest ID of Images in database.
+ *
+ * @async @function getLastImageID
+ * @return {number} number of endpoint last ID
+ */
+export const getLastImageID = async () => {
+    const imageCount = await sequelize.query(
+        'SELECT coalesce(max(id), 0) AS count FROM "images";',
+        { type: QueryTypes.SELECT }
+    )
+    return imageCount[0].count
 }
